0d44c8105bca8fa272074f26a69be0e1c88bef9e
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2023-08-25 16:15:43 -0300

    feat: UserModel

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserModel = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true
+  });
+
+  return UserModel;
+} 
