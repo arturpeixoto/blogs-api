b98efba818194615f7949f6e021844ccc9393a33
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2023-08-25 18:11:50 -0300

    refactor: checkloginfields

--- src/middlewares/checkLoginFields.js
+++ src/middlewares/checkLoginFields.js
@@ -3,9 +3,11 @@ const checkRequiredFields = require('../utils/checkRequiredFields');
 const validateLoginFields = (req, res, next) => {
   const { body } = req;
   const requiredFields = ['email', 'password'];
-  for (let i = 0; i < body.length; i += 1) {
-    const productError = checkRequiredFields(body[i], requiredFields);
-    if (productError) return res.status(400).json({ message: productError });
+  const loginError = checkRequiredFields(body, requiredFields);
+  if (loginError) return res.status(400).json({ message: loginError });
+  const { email, password } = body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
   }
 
   next();
--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { validateLoginFields } = require('../middlewares/checkLoginFields');
 const { loginController } = require('../controllers');
 
 const route = express.Router();
 
-route.post('/', loginController.login);
+route.post('/', validateLoginFields, loginController.login);
 
 module.exports = route;
